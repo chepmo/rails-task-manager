aac5e0f27e7cadb1e0ce37b45df00c67a65b4d3b
Morgane Le Deu <chep.mo@hotmail.fr>
2022-11-15 16:45:43 +0100

    appli ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  # root for create
+  get '/tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # root for delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
+  # root for edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # pour eviter pb new id
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
